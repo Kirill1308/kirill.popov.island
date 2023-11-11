9b760a7e1204ec6a7870ef796a0d2f7e2c95966d
Kirill Popov <kpopov151@gmail.com>
2023-11-11 17:10:38 +0200

    I have just improved StatisticViewProvider (2)

--- src/printer/StatisticViewProvider.java
+++ src/printer/StatisticViewProvider.java
@@ -18,13 +18,27 @@ public class StatisticViewProvider {
 
     public synchronized void printCellCoordinatesAndOrganismCounts(List<Animal> animals, List<Plant> plants, IslandCell islandCell) {
         System.out.printf("(%d, %d)\t", islandCell.getX(), islandCell.getY());
-
-        printOrganismCount(animals, Wolf.class);
-        printOrganismCount(animals, Bear.class);
-        printOrganismCount(animals, Rabbit.class);
-        printOrganismCount(animals, Horse.class);
-        printOrganismCount(plants, Grass.class);
+        printAllOrganismCounts(animals, plants);
     }
+
+    private void printAllOrganismCounts(List<Animal> animals, List<Plant> plants) {
+        printOrganismCountAndEmoji(animals, Wolf.class);
+        printOrganismCountAndEmoji(animals, Bear.class);
+        printOrganismCountAndEmoji(animals, Rabbit.class);
+        printOrganismCountAndEmoji(animals, Horse.class);
+        printOrganismCountAndEmoji(plants, Grass.class);
+    }
+
+    private void printOrganismCountAndEmoji(List<? extends Organism> organisms, Class<? extends Organism> organismType) {
+        long count = countEntitiesOfType(organisms, organismType);
+        String emoji = propertyReader.loadIcon(organismType.getSimpleName());
+        System.out.printf("%s(%d)\t", emoji, count);
+    }
+
+    private long countEntitiesOfType(List<? extends Organism> entities, Class<? extends Organism> entityType) {
+        return entities.stream().filter(entityType::isInstance).count();
+    }
+
 
     private void printOrganismCount(List<? extends Organism> entities, Class<? extends Organism> entityType) {
         int count = countOrganismsOfType(entities, entityType);
@@ -39,12 +53,16 @@ public class StatisticViewProvider {
 
     public void printAnimalAndPlantDetails(List<Animal> animals, List<Plant> plants, IslandCell islandCell) {
         System.out.println("=".repeat(45));
-        System.out.println("Current organisms in cell [" + islandCell.getX() + "][" + islandCell.getY() + "]" + ":");
-        for (Plant plant : plants) {
-            System.out.println(plant.getIcon() + " id = " + plant.getId());
-        }
-        for (Animal animal : animals) {
-            System.out.println(animal.getIcon() + " id = " + animal.getId());
+        System.out.println("Current organisms in cell [" + islandCell.getX() + "][" + islandCell.getY() + "]:");
+
+        printOrganismsDetails(plants);
+        printOrganismsDetails(animals);
+    }
+
+    private void printOrganismsDetails(List<? extends Organism> organisms) {
+        for (Organism organism : organisms) {
+            String details = organism.getIcon() + " id = " + organism.getId();
+            System.out.println(details);
         }
     }
 
@@ -59,18 +77,12 @@ public class StatisticViewProvider {
         }
     }
 
-    private int countOrganismsOfType(List<? extends Organism> entities, Class<? extends Organism> entityType) {
-        int count = 0;
-        for (Organism entity : entities) {
-            if (entityType.isInstance(entity)) {
-                count++;
-            }
-        }
-        return count;
-    }
-
     public int countAnimalsOfType(List<Animal> animals, Class<? extends Animal> animalType) {
         return countOrganismsOfType(animals, animalType);
+    }
+
+    private int countOrganismsOfType(List<? extends Organism> entities, Class<? extends Organism> entityType) {
+        return (int) entities.stream().filter(entityType::isInstance).count();
     }
 
 }
